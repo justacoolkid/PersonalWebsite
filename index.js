cca99a27a0851185bc1979c1a85625bdbdda9bdb
justacoolkid <107064644+justacoolkid@users.noreply.github.com>
2022-06-09 21:35:50 +0530

    Just a personal website
    Basic timepass during summer holidays

---
+++ index.js
@@ -0,0 +1,30 @@
+// LANDING PAGE ANIMATION
+var scene = document.getElementById("scene");
+var parallaxInstance = new Parallax(scene);
+
+var keys = [
+  "Mercury",
+  "Venus",
+  "Earth",
+  "Mars",
+  "Jupiter",
+  "Saturn",
+  "Uranus",
+  "Neptune"
+];
+var slider = new Swiper(".swiper-container", {
+  // Optional parameters
+  slidesPerView: "auto",
+  spaceBetween: 150,
+  centeredSlides: true,
+  mousewheel: true,
+  pagination: {
+    el: ".planet-links",
+    clickable: true,
+    renderBullet: function(index, className) {
+      return '<div class="' + className + '">' + keys[index] + "</div>";
+    }
+  }
+});
+
+
